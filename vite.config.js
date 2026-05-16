31499a6b0005d1afa972cff09fe5baaf85801ff5
ImpanaG-7 <impanag23032007@gmail.com>
2026-05-16 15:32:03 +0530

    Fix vite config base path

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-'/smart-waste/'
+
 export default defineConfig({
   plugins: [react()],
+  base : '/smart-waste/',
 })
